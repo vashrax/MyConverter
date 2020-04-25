ad2c3b0a5a8c899789d5122fc6545d84e35694ab
Grzegorz Ciosek <gregoriciosek@gmail.com>
2020-04-25 15:56:51 +0200

    Fixed currency counting.

--- Currency.java
+++ Currency.java
@@ -12,10 +12,10 @@ class Currency {
 	
 
 	String pound2PLN(double pound) { //funt 5,04
-		return new DecimalFormat("#0.00").format(pound/Conversions.pound2zl);
+		return new DecimalFormat("#0.00").format(pound*Conversions.pound2zl);
 	}
 	
 	String euro2PLN(double euro) { // euro 4,33
-		return new DecimalFormat("#0.00").format(euro/Conversions.euro2zl); 
+		return new DecimalFormat("#0.00").format(euro*Conversions.euro2zl); 
 	}
 }
